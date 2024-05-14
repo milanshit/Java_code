9cd8c19f68503718a677c8c3b148a2b101a91b21
milanshit <127031230+milanshit@users.noreply.github.com>
2024-05-14 18:35:22 +0530

    Lcm.java

--- Lcm.java
+++ Lcm.java
@@ -6,8 +6,8 @@ public class Lcm {
     public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter two number: ");
-		int a=sc.nextInt();
-		int b=sc.nextInt();
+		int a=args[0];
+		int b=args[1];
 		int g=0;
 		for(int i=1;i<=a;i++) {
 			if(a%i==0&&b%i==0)
