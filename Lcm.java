3bc5b3219103f27c779647e8a02c201179f4862b
milanshit <127031230+milanshit@users.noreply.github.com>
2024-05-14 19:25:28 +0530

    Update Lcm.java

--- Lcm.java
+++ Lcm.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Lcm {
     public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter two number: ");
-		int a=args[0];
-		int b=args[1];
-		int g=0;
-		for(int i=1;i<=a;i++) {
-			if(a%i==0&&b%i==0)
-				g=i;
-		}
-		int lcm=a*b/g;
-		System.out.println("LCM= "+lcm);
-
-        sc.close();
+		// Scanner sc=new Scanner(System.in);
+		// System.out.print("Enter two number: ");
+		// int a=args[0];
+		// int b=args[1];
+		// int g=0;
+		// for(int i=1;i<=a;i++) {
+		// 	if(a%i==0&&b%i==0)
+		// 		g=i;
+		// }
+		// int lcm=a*b/g;
+		// System.out.println("LCM= "+lcm);
+		System.out.println("Hello Users");
+			
+        //sc.close();
     }
 }
